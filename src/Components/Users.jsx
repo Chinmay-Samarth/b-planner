85700bb875d99a62ff14ce9bbdb9e6377cdbc907
Chinmay <chinmaydhondale@gmail.com>
2023-04-20 14:29:22 +0530

    20/4

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,7 +1,8 @@
-import { User, useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 export const Users = () => {
+  const {} = useAuth0();
   return (
     <h1>Users</h1>
   );
